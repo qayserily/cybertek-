4b0a64c665be3370621808fc03aba8502a6dfd6f
qayserily <74465236+qayserily@users.noreply.github.com>
2021-04-17 16:09:01 +0300

    Update and rename README.md to Assignment2/Question_1.java

---
+++ Assignment2/Question_1.java
@@ -0,0 +1,8 @@
+public class Question_1 {
+    public static void main(String[] args) {
+        /* Declare and initialize an integer variable called count,
+        And write a statement that displays the value of count on the console. */
+        int count = 25 ;
+        System.out.println(count);
+    }
+}
